702c5064f8ce04bdf5c4dcbd99d9ab147ef84c99
yahyaelsf <yahyaelsaftawi98@gmail.com>
2026-09-09 13:42:46 +0300

    lec1

---
+++ lec1.py
@@ -0,0 +1,34 @@
+# name = '''
+# deddd
+# dddd
+# '''
+# print(name)
+
+# first_name = "yahya"
+# last_name = "elsafawti"
+# full_name = first_name + " "+ last_name
+# age = 25
+# info = "my name is " + full_name + "my age is " + str(age)
+# more_info = f"my name is {full_name} , and my age is {age}"
+# print(info)
+# print(f"my name is {full_name} , and my age is {age}")
+# print(type(True))
+# x = 123.0
+# a = int(x)
+# print(a)
+# age = input("Enter your age :")
+# age_in_months = int(age) * 12
+# print(f"age in months equel {age_in_months}")
+# x = 7 % 2  
+# print(x)
+# num1 = input("Enter your first number :")
+# ope = input("Enter your operation :")
+# num2 = input("Enter your second number :")
+# equation = num1 + ope + num2
+# print(eval(equation))
+# x = 10
+# y = 2.1
+# import math 
+# print(min(10 , 20 , 30))
+# name = "yahyapsd.vpsd.v"
+# print(name[1:10:2]) 
